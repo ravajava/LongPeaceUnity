4575834e116b9da892396ff65b7f02d5492740fe
josue-lopes <josue.lopes258@gmail.com>
2018-09-25 23:36:25 -0400

    Fleshed out TileGraph.cs
    Added more functionality to TileGraph class.

--- AF3DProj/Assets/Scripts/TileGraph.cs
+++ AF3DProj/Assets/Scripts/TileGraph.cs
@@ -8,38 +8,72 @@ using UnityEngine;
  * Date: September 25th, 2018
 */
 
-public class TileGraph : MonoBehaviour {
+public class TileGraph
+{
+    private class TileNode
+    {
+        public List<int> adjagentNodes;     // graph indices of adjacent nodes
+        public int dataIndex;               // index for game data container
 
+        TileNode()
+        {
+            adjagentNodes = new List<int>();
+        }
+    }
 
-    private Vector2 TileVertices;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private List<TileNode> m_TileNodes;
 
     //Creates graph structure using information from source 
     void CreateGraph()
     {
-
+        
     }
 
     //Adds an edge between two vertices
-    void AddEdge(float x, float y, int edge)
+    void AddEdge(int tileAIndex, int tileBIndex)
     {
+        m_TileNodes[tileAIndex].adjagentNodes.Add(tileBIndex);
+        m_TileNodes[tileBIndex].adjagentNodes.Add(tileAIndex);
 
+        // sort adjacent nodes so we can use binary search
+        if (m_TileNodes[tileAIndex].adjagentNodes.Count > 0)
+            m_TileNodes[tileAIndex].adjagentNodes.Sort();
+
+        if (m_TileNodes[tileBIndex].adjagentNodes.Count > 0)
+            m_TileNodes[tileBIndex].adjagentNodes.Sort();
     }
 
     // Removes an edge between two vertices
-    void RemoveEdge(float x, float y)
+    void RemoveEdge(int tileAIndex, int tileBIndex)
     {
+        // search if edge exists
+        int edgeIndexA =  m_TileNodes[tileAIndex].adjagentNodes.BinarySearch(tileBIndex);
+        int edgeIndexB = m_TileNodes[tileBIndex].adjagentNodes.BinarySearch(tileAIndex);
 
+        // if it does, remove edge and resort
+        if (edgeIndexA >= 0 && edgeIndexB >= 0)
+        {
+            m_TileNodes[tileAIndex].adjagentNodes.RemoveAt(edgeIndexA);
+            m_TileNodes[tileBIndex].adjagentNodes.RemoveAt(edgeIndexB);
+
+            // sort adjacent nodes so we can use binary search
+            if (m_TileNodes[tileAIndex].adjagentNodes.Count > 0)
+                m_TileNodes[tileAIndex].adjagentNodes.Sort();
+
+            if (m_TileNodes[tileBIndex].adjagentNodes.Count > 0)
+                m_TileNodes[tileBIndex].adjagentNodes.Sort();
+        }
     }
 
     //Determines/tests wherever an edge exists between two vertices
-    void Adjacent(float x, float y)
+    bool Adjacent(int tileAIndex, int tileBIndex)
     {
+        int edgeIndex = m_TileNodes[tileAIndex].adjagentNodes.BinarySearch(tileBIndex);
 
+        if (edgeIndex >= 0)
+            return true;
+        else
+            return false;
     }
 
     //Lists all nodes y adjacent to a given node x if they exist
