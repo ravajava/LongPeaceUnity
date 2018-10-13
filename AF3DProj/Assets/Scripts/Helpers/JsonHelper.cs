559bab4a00b543d5f1d7a8a7128a3958fc72d34c
josue-lopes <josue.lopes258@gmail.com>
2018-10-13 01:17:57 -0400

    Added helper function for loading battalion data

--- AF3DProj/Assets/Scripts/Helpers/JsonHelper.cs
+++ AF3DProj/Assets/Scripts/Helpers/JsonHelper.cs
@@ -13,13 +13,19 @@ namespace JsonHelpers
     {
         public static List<TileDataWrapper> ParseJsonToTileData(string json)
         {
-            ListWrapper tileWrapper = JsonUtility.FromJson<ListWrapper>(json);
+            TileListWrapper tileWrapper = JsonUtility.FromJson<TileListWrapper>(json);
             return tileWrapper.tiles;
+        }
+
+        public static List<BattalionDataWrapper> ParseJsonToBrigadeData(string json)
+        {
+            BattalionListWrapper battalionWrapper = JsonUtility.FromJson<BattalionListWrapper>(json);
+            return battalionWrapper.battalions;
         }
     }
 
     [System.Serializable]
-    public class ListWrapper
+    public class TileListWrapper
     {
         public List<TileDataWrapper> tiles;
     }
@@ -30,5 +36,38 @@ namespace JsonHelpers
         public int ID;
         public string name;
         public List<int> edges;
+    }
+
+    //[System.Serializable]
+    //public class BrigadeListWrapper
+    //{
+    //    public List<BrigadeDataWrapper> brigades;
+    //}
+
+    //[System.Serializable]
+    //public class BrigadeDataWrapper
+    //{
+    //    public int tileID;
+    //    public string name;
+    //    public List<BattalionDataWrapper> battalions;
+    //}
+
+    [System.Serializable]
+    public class BattalionListWrapper
+    {
+        public List<BattalionDataWrapper> battalions;
+    }
+
+    [System.Serializable]
+    public class BattalionDataWrapper
+    {
+        public int cost;
+        public int lightAtk;
+        public int heavyAtk;
+        public int health;
+        public int armour;
+        public int supply;
+        public int speed;
+        public int awareness;
     }
 }
